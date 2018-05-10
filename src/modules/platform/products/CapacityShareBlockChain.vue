08f9962be7dca0f0fe646501c9dcf0a2a06b59ae
zourui <zourui@mrray.cn>
2018-05-10 21:19:23 +0800

    修改

---
+++ src/modules/platform/products/CapacityShareBlockChain.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "capacity-share-block-chain"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/modules/platform/products/GovernmentBlockChain.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "government-block-chain"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
