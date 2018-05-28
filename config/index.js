e89998ebaf9a9b97d34f539d034d9f7cb58fb59c
zourui <18728053221@163.com>
2018-05-28 14:32:05 +0800

    增加ueditor组件

--- config/index.js
+++ config/index.js
@@ -10,10 +10,16 @@ module.exports = {
         // Paths
         assetsSubDirectory: 'static',
         assetsPublicPath: '/',
-        proxyTable: {},
+        proxyTable: {
+            '/': {
+                target: 'http://localhost:3000',
+                changeOrigin: true,
+                pathRewrite: {'^/': ''}
+            }
+        },
 
         // Various Dev Server settings
-        host: '0.0.0.0', // can be overwritten by process.env.HOST
+        host: 'localhost', // can be overwritten by process.env.HOST
         port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
         autoOpenBrowser: false,
         errorOverlay: true,
--- src/modules/admin/Admin.vue
+++ src/modules/admin/Admin.vue
@@ -1,10 +1,30 @@
 <template>
-
+    <div>
+        <UEditor :defaultMsg=defaultMsg :config=config ref="ue"/>
+        <button @click="getUEContent()">获取内容</button>
+    </div>
 </template>
 
 <script>
+    import UEditor from '../../components/UEditor'
     export default {
-        name: "admin"
+        name: "admin",
+        components: {UEditor},
+        data() {
+            return{
+                defaultMsg: '这里是UE测试',
+                config: {
+                    initialFrameWidth: 1200,
+                    initialFrameHeight: 400
+                }
+            }
+        },
+        methods: {
+            getUEContent() {
+                let content = this.$refs.ue.getUEContent();
+                console.log(content)
+            }
+        }
     }
 </script>
 
